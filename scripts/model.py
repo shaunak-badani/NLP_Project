7811cfa77d5b237c52aa9694ae807aca72670b83
Shaunak Badani <shaunak.badani@duke.edu>
2025-03-15 23:41:45 -0400

    Added model saving

--- scripts/model.py
+++ scripts/model.py
@@ -2,6 +2,7 @@ from make_dataset import DatasetProcessor
 from torch.optim import AdamW
 from transformers import DataCollatorForLanguageModeling, GPT2Tokenizer, \
          GPT2LMHeadModel, TrainingArguments, Trainer
+import torch
 
 class ModelTrainer:
     
@@ -26,32 +27,27 @@ class ModelTrainer:
           num_train_epochs):
       tokenizer = GPT2Tokenizer.from_pretrained(model_name)
       train_dataset = DatasetProcessor.load_dataset(train_file_path, tokenizer)
-      data_collator = ModelTrainer.load_data_collator(tokenizer)
-            
+      data_collator = ModelTrainer.load_data_collator(tokenizer) 
       model = GPT2LMHeadModel.from_pretrained(model_name)
-
       training_args = TrainingArguments(
          output_dir = "/tmp",
          save_strategy = "no",
          per_device_train_batch_size=per_device_train_batch_size,
          num_train_epochs=num_train_epochs
       )
-
       optimizer = AdamW(
          ModelTrainer.get_parameters_to_train(model), 
          lr = 1e-5)
-
       trainer = Trainer(
                model=model,
                args=training_args,
                data_collator=data_collator,
                train_dataset=train_dataset,
                optimizers = (optimizer, None)
-      )
-         
+      ) 
       trainer.train()
+      torch.save(model, "./models/fine_tuned_model.pt")
    
-
 
 if __name__ == "__main__":
    model_name = 'gpt2'
@@ -62,6 +58,6 @@ if __name__ == "__main__":
    train_dataset = DatasetProcessor.load_dataset(train_file_path, tokenizer)
 
    per_device_train_batch_size = 8
-   num_train_epochs = 5.0
+   num_train_epochs = 1.0
    ModelTrainer.train(train_file_path, model_name, per_device_train_batch_size, num_train_epochs)
 
